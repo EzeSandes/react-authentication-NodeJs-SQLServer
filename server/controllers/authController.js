95924a5c45acb1449e7a83c6961b63b2c3afadcc
Eze Sandes <jezesandes@gmail.com>
2022-03-26 19:54:31 -0300

    feat: Added hashing passwords

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -13,7 +13,6 @@ exports.signup = async (req, res) => {
 
     // Hash the password before save it.
     password = await bcrypt.hash(password, 12);
-    console.log(password);
 
     //  Take care about the '' in username and password
     const result = await pool
@@ -41,26 +40,32 @@ exports.signup = async (req, res) => {
 exports.login = async (req, res) => {
   try {
     const { username, password } = req.body;
+
     if (!username || !password)
       throw new Error(`Please provide email and password`);
 
     const pool = await getConnection();
-    //  Take care about the '' in username and password
+
+    //  Take care about the '' in username(VARCHAR)
     const result = await pool
       .request()
       .query(
-        `SELECT id, username FROM ${process.env.DB_USERNAME_TABLE} WHERE username = '${username}' AND password = '${password}'`
+        `SELECT * FROM ${process.env.DB_USERNAME_TABLE} WHERE username = '${username}';`
       );
+    console.log(result);
+    const user = result.recordset[0];
+    console.log('user:', user);
 
-    if (!result.rowsAffected[0] || result.recordset[0].password !== password)
+    if (!user || !(await correctPassword(password, user.password)))
       throw new Error('Incorrect username or password');
     // 401: Error for user not found
 
-    console.log(result);
     res.status(200).json({
       status: 'success',
-      data: result.recordset[0],
-      // data: result.recordset[0], // Send info of the user found
+      data: {
+        id: user.id,
+        username: user.username,
+      },
     });
   } catch (err) {
     console.log(`⛔⛔⛔ LOGIN: ${err.message}`);
@@ -70,3 +75,9 @@ exports.login = async (req, res) => {
     });
   }
 };
+
+/***************** AUTH CONTROLLER UTILITIES */
+/******************************************* */
+async function correctPassword(candidatePassword, userPassword) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+}
